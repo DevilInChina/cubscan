d23d672a327130bbe240d9f36ac4089964023be9
yxgao <905320627@qq.com>
2022-03-09 16:13:47 +0800

    add test code

--- main.cu
+++ main.cu
@@ -2,40 +2,45 @@
 #include "do_scan.cuh"
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
+#include <ctime>
+#include <sys/time.h>
 #include <iostream>
 
 
 using namespace std;
-#define test_num_items (128*1024)
-int main() {
-    thrust::host_vector<int> h_vec(test_num_items);
-    thrust::host_vector<int> h_vec_flag(test_num_items);
+#define test_type int
+int main(int argc,char **argv) {
+    int test_num_items = atoi(argv[1]);      // e.g., 7
+    thrust::host_vector<test_type> h_vec(test_num_items);
+    thrust::host_vector<test_type> h_vec_flag(test_num_items);
 
     for (int i = 0; i < test_num_items; ++i) {
         h_vec[i] = 1;
     }
-    thrust::device_vector<int> d_vec(h_vec);
-    thrust::device_vector<int> d_vec_flag(h_vec_flag);
+    thrust::device_vector<test_type> d_vec(h_vec);
+    thrust::device_vector<test_type> d_vec_flag(h_vec_flag);
 
 
-    thrust::device_vector<int> d_vec_res(test_num_items);
+    thrust::device_vector<test_type> d_vec_res(test_num_items);
     cudaStream_t cudaStream;
     cudaStreamCreate(&cudaStream);
     void *temp = nullptr;
-    unsigned int temp_size = scan::get_temp_storage_size<int>(test_num_items);
-    cudaMalloc(&temp,temp_size);
+    unsigned int temp_size = scan::get_temp_storage_size<test_type>(test_num_items);
+    cudaMalloc(&temp, temp_size);
     cudaDeviceSynchronize();
-    scan::ExclusiveScan(temp,d_vec.data().get(), d_vec_res.data().get(), test_num_items,cudaStream);
-    thrust::host_vector<int> h_vec_res(test_num_items);
-
+    struct timeval t1, t2;
+    gettimeofday(&t1, nullptr);
+    for (int i = 0; i < 10; ++i) {
+        scan::ExclusiveScan(temp, d_vec.data().get(), d_vec_res.data().get(), test_num_items, cudaStream);
+    }
+    cudaStreamSynchronize(cudaStream);
+    gettimeofday(&t2, nullptr);
+    thrust::host_vector<test_type> h_vec_res(test_num_items);
+    double time = 1000 * (t2.tv_sec - t2.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000.0;
+    cout << test_num_items * sizeof(test_type) * 10 / (time *1e6) << endl;
     cudaDeviceSynchronize();
     h_vec_flag = d_vec_res;
     cudaDeviceSynchronize();
-    int cnt = 0;
-    for (int i = 0; i < test_num_items; ++i) {
-        if(d_vec_res[i] != i && ++cnt < 20)
-            cout << i << "\t" << h_vec[i] << "\t" << d_vec_res[i] << "\n";
-    }
 
     return 0;
 }
---
+++ test_cub.cu
@@ -0,0 +1,45 @@
+//
+// Created by yxgao on 2022/3/9.
+//
+
+#include <cub/cub.cuh>   // or equivalently <cub/device/device_scan.cuh>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <sys/time.h>
+#include <iostream>
+// Declare, allocate, and initialize device-accessible pointers for input and output
+#define test_type int
+using namespace std;
+int main(int argc ,char **argv) {
+    int test_num_items = atoi(argv[1]);      // e.g., 7
+    int *d_in;          // e.g., [8, 6, 7, 5, 3, 0, 9]
+    int *d_out;         // e.g., [ ,  ,  ,  ,  ,  ,  ]
+    thrust::host_vector<test_type> h_vec(test_num_items);
+    thrust::host_vector<test_type> h_vec_flag(test_num_items);
+
+    for (int i = 0; i < test_num_items; ++i) {
+        h_vec[i] = 1;
+    }
+    thrust::device_vector<test_type> d_vec(h_vec);
+    thrust::device_vector<test_type> d_vec_flag(h_vec_flag);
+    d_in = d_vec.data().get();
+    d_out = d_vec_flag.data().get();
+// Determine temporary device storage requirements
+    void *d_temp_storage = NULL;
+    size_t temp_storage_bytes = 0;
+    cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, d_in, d_out, test_num_items);
+// Allocate temporary storage
+    cudaMalloc(&d_temp_storage, temp_storage_bytes);
+// Run exclusive prefix sum
+
+    struct timeval t1, t2;
+    gettimeofday(&t1, nullptr);
+    for (int i = 0; i < 10; ++i) {
+        cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, d_in, d_out, test_num_items);
+    }
+    cudaDeviceSynchronize();
+    gettimeofday(&t2, nullptr);
+    double time = 1000 * (t2.tv_sec - t2.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000.0;
+    cout << test_num_items * sizeof(test_type) * 10 / (time *1e6) << endl;
+// d_out s<-- [0, 8, 14, 21, 26, 29, 29]
+}
